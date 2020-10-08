3960494f5674ec3f2565593fcfd42b0c648b67e1
DemonWang <nanwanwang@hotmail.com>
2020-10-08 10:00:04 +0800

    Update Person.java
    www

--- src/com/demon/Person.java
+++ src/com/demon/Person.java
@@ -4,6 +4,6 @@ public class Person {
 
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("from java  eee");
+      333333
     }
 }
